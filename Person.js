031a733a7e326e1a18e0475beca112cb9cff4632
PriyanshiPrateek812 <154115675+PriyanshiPrateek812@users.noreply.github.com>
2025-02-20 23:09:08 +0530

    Person.js

---
+++ Person.js
@@ -0,0 +1,9 @@
+const Person=(props)=>{
+    return (
+        <div>
+            <h1>My name is {props.name}</h1>
+            <p>My age is {props.age}</p>
+        </div>
+    )
+}
+export default Person;
