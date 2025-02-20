2ef0cc768603e98f63b659cb3a47caa546854349
PriyanshiPrateek812 <154115675+PriyanshiPrateek812@users.noreply.github.com>
2025-02-20 23:09:35 +0530

    Create Employee.js

---
+++ Employee.js
@@ -0,0 +1,8 @@
+const Employee = (props) => {
+    return (
+        <div>
+        <h1>Id: {props.id}, Name: {props.name}, EmailId: {props.email} </h1>
+        </div>
+    );
+}
+export default Employee;
